aa3dae51140b0f43c918b1ed4591e4e597bb7b31
VGFenrir <119891860+VGFenrir@users.noreply.github.com>
2025-02-27 14:42:07 +0100

    Update Segment.java

--- SVG/src/Segment.java
+++ SVG/src/Segment.java
@@ -1,9 +1,14 @@
 public class Segment {
     public Point p1,p2;
 
+//    public Segment(Point p1_, Point p2_){
+//        this.p1 = p1_;
+//        this.p2 = p2_;
+//    }
+    
     public Segment(Point p1_, Point p2_){
-        this.p1 = p1_;
-        this.p2 = p2_;
+        this.p1 = new Point (p1_.x, p2_.y);
+        this.p2 = new Point (p2_.x, p2_.y);
     }
 
     public double length(){
